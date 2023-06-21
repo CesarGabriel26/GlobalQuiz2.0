0ce531b980cc165efe5a6c325694466eed1dc77b
Cesar Gabriel <cesargabrielsousasousa196@gmail.com>
2023-06-21 15:32:10 -0300

    atualização jogo,editor e comentarios explicativos

--- js/CriarQuiz.js
+++ js/CriarQuiz.js
@@ -16,11 +16,17 @@ let Certo_RespostaB = document.getElementById('Certo_RespostaB')
 let Certo_RespostaC = document.getElementById('Certo_RespostaC')
 let Certo_RespostaD = document.getElementById('Certo_RespostaD')
 
+//Esta variavel guardara o caminho/link da iamgem escolhida
 var FilePath = ""
+//Esta varaivel armazena as informações das paginas do quiz
 var QuizData = []
+//Esta guarda a quantidade de paginas
 var cont_pagias = 0
+//Esta guarda qual é a pagina atual
 var PaginaAtual = 0
-var EmptyData = {
+
+//esta constante guarda o formato de pagina de um quiz vasio
+const EmptyData = {
     Pergunta: "",
     Img: "",
     Respsotas: [
@@ -31,39 +37,46 @@ var EmptyData = {
     ]
 }
 
+//Aqui carregamos todos os quizes no do local storage, se for vasio pegamos os dados do banco de perguntas
+var AllQuizes = JSON.parse(localStorage.getItem('GlobalQuiz_Quizes')) || BancoPErguntas()
 
-var AllQuizes = []
-AllQuizes = JSON.parse(localStorage.getItem('GlobalQuiz_Quizes'))
-if (AllQuizes == null) {
-    AllQuizes = []
-}
-console.log(AllQuizes);
-
-
+//Aqui vemos se a imagem foi clicada
 Imagem.addEventListener('click', () => {
+    //se sim chamamos o Input de imagem e resetamos se ele tiver algum arquivo
     ImagemINP.click()
     ImagemINP.files[0] = ""
 })
 
+//Chacamos se A um input de imagem
 if (ImagemINP) {
+    //se ouver checamos se o valor nele mudou
     ImagemINP.addEventListener('change', e => {
+        //Se mudou armazenamos este valor 
         const file = ImagemINP.files[0]
+        //Criamos um "Leitor de Arquivos"
         const reader = new FileReader();
 
+        //Checamos se ele ja carregou
         reader.addEventListener('load', () => {
+            //Se sim armazenamos o link gerado
             FilePath = reader.result
             Imagem.src = FilePath
 
         })
+        //Aqui chamamos o Leitor com a função para trasformar o arquivo em um Link
         reader.readAsDataURL(file);
     })
 }
 
+//Esta função não é obrigatoria mais a fiz para sempre estar atualizando a UI do site 
+//(User interface || Interface de usuario)
 setInterval(() => {
     LoadUI()
 }, 100);
 
+//Esta função é chamada quando o usuario clica no + para adicionar uma nova pagina
 function AddPage() {
+    //Salvamos as informações da pagina em um Objeto
     var Data = {
         Pergunta: Pergunta.value,
         Img: FilePath,
@@ -75,69 +88,112 @@ function AddPage() {
         ]
     }
 
+    //Verificamos se tudo esta preenchido
     if (Validar()) {
+        //Se sim armazenamos a pagiana
         QuizData[PaginaAtual] = Data
+
+        //e almentamos a conagem de pagians e a pagina atual EX:
+        /* 
+            Valor inicial:
+            cont_pagias = 0
+            PaginaAtual = 0
+
+            Adicionamos
+            cont_pagias++
+            PaginaAtual++
+
+            Valor final
+            cont_pagias = 1
+            PaginaAtual = 1
+        */
         cont_pagias++
         PaginaAtual++
+        //Esta função limpa todos os inputs para criar uma nova pagina
         Clear()
     } else {
-        console.log(Data.Respsotas);
+        //se não damos um alerta para o usuario preencher todas os inputs requisitados
         alert('Preencha os campos obrigatorios[Respsota correta, pergunta e respsotas]')
     }
 
 }
 
+//Esta função é chamada quando o usuario clica no - para apagar a pagina atual
 function RemovePage() {
+    //Primeiro chegamos se a contagem de paginas é maior que 0
     if (cont_pagias > 0) {
+        //Se sim Apagamos a pagina com a função splice do Array QuizData
         QuizData.splice(PaginaAtual, 1)
+        // e diminuimos a quantidade de paginas
         cont_pagias--
+        //E carregamos a pagina que estava na frente da pagina deletada
         Load(QuizData[cont_pagias])
     }
 }
 
+//Esta função é chamada quando criamos uma nova pagina
 function Validar() {
+    //Checamos se a pergunta esta vasia, se estiver retornamos falso
     if (Pergunta.value == "") {
         return false
     }
+    //Checamos se a resposta certa foir marcada, se não for retornamos falso
     if (Certo_RespostaA.checked == false && Certo_RespostaB.checked == false && Certo_RespostaC.checked == false && Certo_RespostaD.checked == false) {
         return false
     }
+    //Checamos se as respostas foram preenchidas, se não for retornamos falso
     if (RespostaA.value == "" && RespostaB.value == "" && RespostaC.value == "" && RespostaD.value == "") {
         return false
     }
+    //Se tudos estiver certo retornamos verdadeiro
     return true
 }
 
+//Esta função é chamada quando criamos uma nova pagina
 function Clear() {
+    //Resumidamente setamos todos os falores como vasio
     RespostaA.value = ""
     RespostaB.value = ""
     RespostaC.value = ""
     RespostaD.value = ""
     FilePath = ""
     Imagem.src = FilePath
-    Certo_RespostaA.checked = false
+    //Somante a respsota A fica verdadeira como padrão
+    Certo_RespostaA.checked = true
     Certo_RespostaB.checked = false
     Certo_RespostaC.checked = false
     Certo_RespostaD.checked = false
     Pergunta.value = ""
 }
 
+//Esta função é chamada toda vez a cada 100 milesegundos
 function LoadUI() {
+    //Ela somente atualiza a contagem de paginas
     NumeroDeQuestoes.innerHTML = `<p>${QuizData.length}</p>`
 }
 
+//Esta função é chamada quando o usuario clica nas setas < e > para avanças ou voltar uma pagina
 function Befote_After_PAge(Value) {
+    //Aqui adicionamos o valor recebido a pagina atual (recebemos 1 ou -1)
     PaginaAtual += Value
+
+    //Checamos se a contagem é menor que 0
     if (PaginaAtual < 0) {
+        //se sim setamos como 0
         PaginaAtual = 0
     } else if (PaginaAtual > (QuizData.length - 1)) {
+        //Checamos se a contagem é manor que a quantidade de paginas ja salvas
+        //e se for setamos como a quantidade total de paginas
         PaginaAtual = (QuizData.length - 1)
     }
-    console.log(PaginaAtual);
+    //e chamamos a função de carregamento com a nova pagina atual
     Load(QuizData[PaginaAtual])
 }
 
+//Esta função carrega a informação do quiz quando a chamamos na remoção,criação, avanço e retorno
 function Load(Data) {
+    //Resumindamente ela seta todos os elementos com as informações salvas que recebemos no parametro
+    //Data
     RespostaA.value = Data.Respsotas[0][0]
     RespostaB.value = Data.Respsotas[1][0]
     RespostaC.value = Data.Respsotas[2][0]
@@ -154,47 +210,68 @@ function Load(Data) {
     Imagem.src = Data.Img
 }
 
+//esta função é chamada quando o usuario clica no martelo para contruir o quiz
 function ValidarBuild() {
-    if (QuizData.length > 0) {
+    //Primeiramente checamos se o tamanho do quiz é maior que 0
+    //Ou seja, se Á pagina salva
+    if (QuizData.length > 1) {
+        //Se sim mostramos a tela de construção
         document.getElementById("Confirm").style.display = 'flex'
     } else {
-        alert('Seu quiz presiza ter ao menos uma pagina')
+        //Se não alertamos para o usuario salvar ao menos duas pagina
+        alert('Seu quiz presiza ter ao menos 2 pagina')
     }
 }
+
 function Voltar() {
+    //Esta função é chamada pela tela de construção para o usuario voltar a tela de edição do quiz
     document.getElementById("Confirm").style.display = 'none'
 }
 
-var QuizImage = document.getElementById('QuizImage')
-
+//E aqui construimos o quiz
 function Build() {
-
+    //Primeiro pegamos o nome e o link para o quiz (link para imagem do quiz se não ouver um a imagem
+    //padrão sera carregada )
     var nome = document.getElementById('QuizName')
     var Link = document.getElementById('LinkImagem')
 
+    //Aqui checamos em toda a informação das paginas do quiz se a alguma vasia
     QuizData.forEach((data, i) => {
         if (data === EmptyData || data.Respsotas === EmptyData.Respsotas || data.Pergunta === EmptyData.Pergunta) {
             QuizData.splice(i, 1)
+            //Se ouver apagamos a pagina vasia
         }
     })
 
+    //Aqui carrecamos a imagem padrão do quiz / capa
+
     var img = "../img/Capa dos quizes.png"
+    
+    //Checamos se o valor do link não é vasio
     if (Link.value != "") {
+        //se tiver algum link o setamos como capa
         img = Link.value
     }
 
+    //checamos se o nome bão esta vasio
     if (nome.value != "") {
+        //se não estiver armazenamos toda a informação em um novo objeto
         var finalData = {
             QuizNome: nome.value,
             Foto: img,
             categoria: Categoria.value,
             quizData: QuizData
         }
+        //Adicionamos ele juntamente com todos os quizes 
         AllQuizes.push(finalData)
+        // e o gardamos
         localStorage.setItem('GlobalQuiz_Quizes', JSON.stringify(AllQuizes))
+        //Alertamos que o quiz foi criado
         alert('Quiz salvo com suceço')
+        //E por fim redirecionamos o usuario a pagina inicial
         location.href = "principal.html"
     } else {
+        //se estiver vasio alertamos ao usuario
         nome.focus()
         alert('De um nome ao seu quiz')
     }
--- js/Jogar.js
+++ js/Jogar.js
@@ -12,117 +12,180 @@ let RespostaC = document.getElementById('RespostaC')
 let RespostaD = document.getElementById('RespostaD')
 
 
-var FilePath = ""
 
+//Variavel onde sera armazenada a informação do quiz que esta sendo jogado
 var QuizData = []
+// Aqui armazenamos as respsotas da pagina atual
 var Respostas = []
+// esta variavel é usada para determinar qual pagina sera carregada na tela
 var PaginaAtual = 0
-//0 Acertos V V Erros 1 //
+
+//Esta variavel recebe todos os usuario cadastrados
+var Users = JSON.parse(localStorage.getItem('GlobalQuizUsers')) || []
+//Ja esta recebe o usuario atual logado
+var usuarioatual = JSON.parse(localStorage.getItem("GlobalQuizUsuarioAtual"))
+
+//     0 Acertos V  V Erros 1 //
 var Pontuacao = [0, 0]
 
-AllQuizes = JSON.parse(localStorage.getItem('GlobalQuiz_Quizes')) || BancoPErguntas()
+// Variavel que impede o usuario de usar mais de uma ajuda na mesma pergunta
+var JaAjudou = false
+// jarespondeu Não permite que o usuario clique varias vezes na mesma pergunta 
+var jarespondeu = false
+// Oneshot Não permite o som de tocar mais de uma vez
+var oneshot = true
+
+//Armazenando o Objeto que esta dentro do local storage na variavel All AllQuizes
+//Se ele for vasio a variavel ira receber o Objeto salvo no Banco de Perguntas
+var AllQuizes = JSON.parse(localStorage.getItem('GlobalQuiz_Quizes')) || BancoPErguntas()
+
+//Chamando a função para carregar o quiz
 
 LoadQuizData()
+
+
 function LoadQuizData() {
+    //Fazendo um loop por todos os quizes registrados
     AllQuizes.forEach(data => {
+        // Se o nome do quiz atual no loop for igual ao nome do quiz que a pessoa clicou (que esta armaze)
+        //nado no local storage 'QuizAtual' ele armazena as informaçoes do quiz em QuizData e chama a função
+        //de carregamento
         if (data.QuizNome == localStorage.getItem('QuizAtual')) {
             QuizData = data.quizData
+            //Chamando a função de carregamento passando como parametro A informação do quiz atual
+            //Iniciando na pagina 0 "PaginaAtual = 0"
             Load(QuizData[PaginaAtual])
         }
     })
-
 }
-var oneshot = true
 
 function Load(Data) {
-
+    //Primeiro verificamos se a pagina atual é igual a nossa quantidade de paginas em QuizData
     if (PaginaAtual == QuizData.length) {
+        // Se for finalizamos o jogo chamando a função FimDeJogo
         FimDeJogo()
     } else {
+        // Se não carregamos todas as informações na tela
+
+        //Aqui carregamos cada Testo da esposta na posição 0
         RespostaA.innerHTML = Data.Respsotas[0][0]
         RespostaB.innerHTML = Data.Respsotas[1][0]
         RespostaC.innerHTML = Data.Respsotas[2][0]
         RespostaD.innerHTML = Data.Respsotas[3][0]
 
+        // Aqui Resetamos so botões desabilitados
+        //No caso esta parte é usada quando o usuario usa uma das ajudas então é necessario o reset
         RespostaA.parentElement.disabled = false
         RespostaB.parentElement.disabled = false
         RespostaC.parentElement.disabled = false
         RespostaD.parentElement.disabled = false
 
-        document.getElementById('Elimina1').disabled = false
-        document.getElementById('Elimina2').disabled = false
+        //document.getElementById('Elimina1').disabled = false
+        //document.getElementById('Elimina2').disabled = false
+        // Aqui resetamos a variavel de ajuda para caso o usuario tenha usado uma
+        JaAjudou = false
 
+        // Aqui carregamos as respostas na variavel de respostas
+        // A jeitos mais faceis porem fiz da maneira que mais entendia
         Respostas[0] = Data.Respsotas[0][1]
         Respostas[1] = Data.Respsotas[1][1]
         Respostas[2] = Data.Respsotas[2][1]
         Respostas[3] = Data.Respsotas[3][1]
 
+        //Aqui carregamos a pergunta 
         Pergunta.innerHTML = Data.Pergunta
 
+        //Aqui verificamos se á imagem nas informações da pagina
         if (Data.Img != "") {
-            Imagem.style.display = "block"
+            //Se sim carregamos a imagem e a mostramos
+            Imagem.style.opacity = "1"
             Imagem.src = Data.Img
         } else {
-            Imagem.style.display = "none"
+            //Se não escondemos a imagem
+            Imagem.style.opacity = "0"
         }
 
+        // E por fim resetamos a variavel OneShot 
         oneshot = true
     }
 }
-var jarespondeu = false
+
 function Responder(ID) {
+    //Quando a função é chamda verificamos se o o usuario ja respondeu
     if (!jarespondeu) {
+        // se não (!jarespondeu é o mesmo que jarespondeu == false]) verificamos a resposta
         if (Respostas[ID]) {
+            //Se esta respsota for a verdadeira adicionamos 1 a contagem de acertos
             Pontuacao[0] += 1
+            //E tocamos o som de acerto
             TocarAuduio('Acerto')
         } else {
+            //Se não adicionamos 1 a contagem de erros
             Pontuacao[1] += 1
+            //E tocamos o som de erro
             TocarAuduio('Errado')
         }
+        // e por fim setamos jarespondeu como true para impedir o usuario de clicar mais de uma vez
         jarespondeu = true
     }
 }
 
 function TocarAuduio(Audio = "", Estado = "") {
+    //Nesta função recebemos o audio e o estado do jogo
+    //então começamos checando se Podemos tocar if (oneshot == true)
     if (oneshot) {
+        //se sim setamos oneshot como false
         oneshot = false
+        // Criamos um elemento de audio
         var a = document.createElement('audio')
+        //Setamos o audio no elemento com o nome do audio recebido
         a.src = `../audio/${Audio}.mp3`
+        // E o tocamos
         a.play()
+        //Verificamos se o audiuo acabou de tocar
         a.addEventListener('ended', () => {
+            //Se sim verificamos o estado do jogo
             if (Estado == "") {
+                // se for igual a vasio passamos apra poxima pagina
                 PaginaAtual++
+                //E chamamos a função de carregamento com as informações da proxima pagina
                 Load(QuizData[PaginaAtual])
+                //E resetamos a ja respondeu
                 jarespondeu = false
             }
         })
     }
 }
 
-var Users = []
-Users = JSON.parse(localStorage.getItem('GlobalQuizUsers'))
-if (Users == null) {
-    Users = []
-}
-var usuarioatual = JSON.parse(localStorage.getItem("GlobalQuizUsuarioAtual"))
-
 
 function FimDeJogo() {
+    //Quando esta função é chamada encerramos o jogo
+    //Resetamos oneshot
     oneshot = true
+    //Pegamos o elemento da tela de fim de jogo e o mostramos trocando seu display de none para flex
     document.getElementById('TelaFInal').style.display = "flex"
 
+    //Checamos qual a maior pontuação
     if (Pontuacao[0] > Pontuacao[1]) {
+        //Se ouver mais acertos deixamos a bandeira verde(de acertos) amior e no topo
+        //e a de erros(vermelha) em baixo e menor
         document.getElementById('BandeiraPrincipal').innerHTML = `<p>${Pontuacao[0]} Acertos</p>`
         document.getElementById('BandeiraSegundaria').innerHTML = `<p>${Pontuacao[1]} Erros</p>`
+        //E tocamos o som de vitoria com o estado 'Fim de jogo' para não adicionar +1 na pagina
         TocarAuduio("Vitoria", 'Fim De Jogo')
     } else {
+        //se ouver mais erros
+        //Trocamos o trofeu inteiro para o quebrado
         document.getElementById('Trofeu').src = "../img/Trofeu derrota.png"
 
+        //setamos a bandeira maior como a de erros
         document.getElementById('BandeiraPrincipal').innerHTML = `<p>${Pontuacao[1]} Erros</p>`
         document.getElementById('BandeiraPrincipal').style.backgroundImage = 'url("../img/bandeira muito erro.png")'
-
+        // e a menor como a de acertos ja que ouveram menos acertos
         document.getElementById('BandeiraSegundaria').innerHTML = `<p>${Pontuacao[0]} Acertos</p>`
         document.getElementById('BandeiraSegundaria').style.backgroundImage = 'url("../img/bandeira muito acerto.png")'
+        
+        //E tocamos o som de derrota com o estado 'Fim de jogo' para não adicionar +1 na pagina
         TocarAuduio("derrota", 'Fim De Jogo')
     }
 
@@ -138,32 +201,77 @@ function FimDeJogo() {
 }
 
 // painelAjuda
+
+//Aqui pegamos o elemento do painel de ajuda
 let painelAjuda = document.getElementById('painelAjuda')
+//Escondemos ele setando seu diplay para none
+painelAjuda.style.display = 'none'
+
+//Esta função é chamada quando clicamos no botão da interrogação
 function MostrarAjuda() {
+    //checamos se o painel esta escondido
     if (painelAjuda.style.display == 'none') {
+        //Se sim o mostramos
         painelAjuda.style.display = 'block'
     } else {
+        //Se não o escondemos novamente
         painelAjuda.style.display = 'none'
     }
 }
 
+//Esta variavel armazena quantas de cada tipo de ajuda o usuario possui
+// no cado 6 que removem uma respsota errada e 5 que removem duas
+var Ajudar = {
+    RemoverUma: 6,
+    RemoverDuas: 5
+}
+
+//Esta variavel armazena qual das perguntas ja foi eliminada 
+var jafoi = []
+
+//Aqui pegamos os textos que mostram a quantidade de cada ajuda
+document.getElementById('QUantidade1').innerHTML = Ajudar.RemoverUma
+document.getElementById('QUantidade2').innerHTML =  Ajudar.RemoverDuas
+
+//Esta função é chamada quando clicamos em uma das ajudas
 function EliminaRespErrada(Quantidade) {
-    for (let num = 0; num < Quantidade; num++) {
-        Elimina()
+    //Resetamos a variavel de ajuda
+    jafoi = []
+
+    //E checamos se o usuario ja foi ajudado
+    if (JaAjudou == false) {
+        //se não setamos a variavel de ja ajudou como true
+        JaAjudou = true
+        //Checamos quantas respsotas teremos que eliminar
+        if (Quantidade == 1) {
+            //Se for sómente uma nos retiramos a quantidade deste tipo de ajuda
+            Ajudar.RemoverUma--
+        }else {
+            //se for duas faremos o mesmo só que com a variavel de duas
+            Ajudar.RemoverDuas--
+        }
+    
+        //Aqui chamamos a vfunção que checa se temos ajudas para dar
+        if (Checar()) {
+            //se sim fazemos um loop pela quantidade que sera removida
+            for (let num = 0; num < Quantidade; num++) {
+                //e chamamos a função de eliminar
+                Elimina()
+            }
+    
+        }
+        //Por fim Escondemos o painel de ajuda
+        MostrarAjuda()
     }
-
-    document.getElementById('Elimina1').disabled = true
-    document.getElementById('Elimina2').disabled = true
-
-    MostrarAjuda()
 }
 
 function Elimina() {
-    var jafoi = []
-
+    //Quando esta função é chamada geramos um numero aleatorio de 0 a 3 (A: 0, B:1, C:2, D:3)
     var i = parseInt(Math.random() * 4)
+    //E checamos se este numero ja foi escolido e se ele não é a respsota correta
     if (jafoi.indexOf(i) == -1 && QuizData[PaginaAtual].Respsotas[i][1] == false) {
-
+        //Se estiver tudo certo checamos qual das respsotas teremos que desabilitar e
+        //a desabilitamos com .disabled = true;
         if (i == 0) {
             RespostaA.parentElement.disabled = true;
         } else if (i == 1){
@@ -174,9 +282,40 @@ function Elimina() {
             RespostaD.parentElement.disabled = true;
         }
 
+        //e adicionamos esta valor a variavel Jafoi para que ele bão se repita
         jafoi.push(i)
-        console.log(i);
     } else {
+        //Se não estiver tudo correto chamamos esta função novamente para gerar um novo numero
         Elimina()
     }
+}
+
+function Checar() {
+    //Quando esta função é chamada 
+
+    //Checamos se a respsotas do tipo 1
+    if (Ajudar.RemoverUma <= 0) {
+        //Se não ouver desabilitamos esta opção
+        document.getElementById('Elimina1').disabled = true
+    }
+
+    //Checamos se a respsotas do tipo 2
+    if (Ajudar.RemoverDuas <= 0) {
+        //Se não ouver desabilitamos esta opção
+        document.getElementById('Elimina2').disabled = true
+    }
+
+    //Checamos se os dois tipos ja foram totalmente usados
+    if (Ajudar.RemoverDuas <= 0 && Ajudar.RemoverUma <= 0) {
+        //Se sim retornamos falso interormpendo a função de ajuda
+        return false
+    }
+
+    //Se tudo estiver correto Setamos os testos de quantidade para cada
+    //Quantidade, QUantidade1,QUantidade2 e RemoverUma, RemoverDuas respectivamente
+    document.getElementById('QUantidade1').innerHTML = Ajudar.RemoverUma
+    document.getElementById('QUantidade2').innerHTML =  Ajudar.RemoverDuas
+
+    //E retornamos verdadeiro
+    return true
 }
